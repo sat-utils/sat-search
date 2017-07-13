1459b609d6424aa76033d0070bbd846a844bf16a
Matthew Hanson <matt.a.hanson@gmail.com>
2017-07-13 16:43:03 -0400

    properly expand user home dir

--- satsearch/config.py
+++ satsearch/config.py
@@ -4,7 +4,7 @@ import os
 SAT_API = os.getenv('SAT_API', 'https://api.developmentseed.org/satellites')
 
 # Data directory to store downloaded imagery
-DATADIR = os.getenv('SATUTILS_DATADIR', '~/satutils-data')
+DATADIR = os.getenv('SATUTILS_DATADIR', os.path.expanduser('~/satutils-data'))
 
 # do not create subdirectories of satellite_name/scene_id when saving imagery
 NOSUBDIRS = False
