a59272feb94013040971ef585881fd425d8516fb
Matthew Hanson <matt.a.hanson@gmail.com>
2017-08-21 18:50:04 -0400

    setup compatible with 2.7

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 from imp import load_source
 from os import path
+import io
 
 __version__ = load_source('satsearch.version', 'satsearch/version.py').__version__
 
 here = path.abspath(path.dirname(__file__))
 
 # get the dependencies and installs
-with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+with io.open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
     all_reqs = f.read().split('\n')
 
 install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
