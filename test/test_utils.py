edaa704f6042e0eb967c463edd0605c00d8ac2f9
Matthew Hanson <matt.a.hanson@gmail.com>
2017-09-06 21:59:58 -0400

    expand test coverage for utils

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,8 +1,34 @@
+import os
 import unittest
+import satsearch.config as config
+from satsearch.scene import Scenes
 
 
 class Test(unittest.TestCase):
 
-    def test_get_text_calendar(self):
-        """ Get text calendar of dates """
-        pass
+    path = os.path.dirname(__file__)
+
+    @classmethod
+    def setUpClass(cls):
+        """ Configure testing class """
+        config.DATADIR = cls.path
+
+    def load_scenes(self):
+        return Scenes.load(os.path.join(self.path, 'scenes.geojson'))
+
+    def test_text_calendar(self):
+        """ Get calendar """
+        scenes = self.load_scenes()
+        cal = scenes.text_calendar()
+        self.assertEqual(len(cal), 1174)
+        self.assertTrue(' 2017 ' in cal)
+        self.assertTrue(' January ' in cal)
+        self.assertTrue(' April ' in cal)
+
+    def test_text_calendar_multiyear(self):
+        scenes = self.load_scenes()
+        scenes[0].metadata['date'] = '2010-02-01'
+        cal = scenes.text_calendar()
+        self.assertEqual(len(cal), 15086)
+        self.assertTrue(' 2016 ' in cal)
+        self.assertTrue(' 2017 ' in cal)
