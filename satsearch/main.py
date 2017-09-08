f9dc8cd5deebf7ed340a7f9ce48897472577127a
Matthew Hanson <matt.a.hanson@gmail.com>
2017-09-08 18:55:59 -0400

    parser moved to own module

--- satsearch/main.py
+++ satsearch/main.py
@@ -1,97 +1,36 @@
 import os
 import sys
-import argparse
-import logging
 import json
 from .version import __version__
 from satsearch import Search, Scenes
-import satsearch.config as config
+from satsearch.parser import SatUtilsParser
 
 
-logging.basicConfig(level=logging.INFO, format='%(message)s')
-logger = logging.getLogger(__name__)
-
-
-class KeyValuePair(argparse.Action):
-    """ Custom action for getting arbitrary key values from argparse """
-    def __call__(self, parser, namespace, values, option_string=None):
-        for val in values:
-            n, v = val.split('=')
-            setattr(namespace, n, v)
-
-
-def parse_args(args):
-    """ Parse arguments for sat-search CLI """
-    desc = 'sat-search (v%s)' % __version__
-    dhf = argparse.ArgumentDefaultsHelpFormatter
-    parser = argparse.ArgumentParser(description=desc, formatter_class=dhf)
-
-    group = parser.add_argument_group('Platform search parameters')
-    group.add_argument('--satellite_name')
-    group.add_argument('--sensor')
-
-    group = parser.add_argument_group('Temporal search parameters')
-    group.add_argument('--date')
-    group.add_argument('--date_from')
-    group.add_argument('--date_to')
-
-    group = parser.add_argument_group('Spatial search parameters')
-    group.add_argument('--intersects', help='GeoJSON Feature (file or string)')
-    group.add_argument('--contains', help='long,lat points')
-
-    group = parser.add_argument_group('Other search parameters')
-    group.add_argument('--scene_id', help='One or more scene IDs', nargs='*', default=None)
-    group.add_argument('--cloud_from', help='Lower limit for cloud coverage')
-    group.add_argument('--cloud_to', help='Upper limit for cloud coverage')
-    group.add_argument('--param', nargs='*', help='Additional parameters of form KEY=VALUE', action=KeyValuePair)
-
-    group = parser.add_argument_group('Output')
-    group.add_argument('--printsum', help='Print basic metadata for matched scenes', default=False, action='store_true')
-    group.add_argument('--printcal', help='Print calendar showing dates', default=False, action='store_true')
-    group.add_argument('--review', help='Interactive review of thumbnails', default=False, action='store_true')
-    group.add_argument('--save', help='Save scenes metadata as GeoJSON', default=None)
-    group.add_argument('--datadir', help='Local directory to save images', default=config.DATADIR)
-    group.add_argument('--nosubdirs', help='When saving, do not create directories usng scene_id',
-                       default=False, action='store_true')
-
-    group = parser.add_argument_group('Download')
-    group.add_argument('--download', help='Download files', default=None, nargs='*')
-    group.add_argument('--source', help='Download source', default='aws_s3')
-
-    args = vars(parser.parse_args(args))
-    args = {k: v for k, v in args.items() if v is not None}
-
-    if 'intersects' in args:
-        if os.path.exists(args['intersects']):
-            with open(args['intersects']) as f:
-                args['intersects'] = json.dumps(json.loads(f.read()))
-
-    return args
-
-
-def main(datadir=config.DATADIR, nosubdirs=config.NOSUBDIRS, printsum=False, printcal=False,
-         review=False, save=None, download=None, source='aws_s3', **kwargs):
+def main(review=False, printsearch=False, printmd=None, printcal=False,
+         save=None, download=None, source='aws_s3', **kwargs):
     """ Main function for performing a search """
-    config.DATADIR = datadir
-    config.NOSUBDIRS = nosubdirs
+    if printsearch:
+        txt = 'Search for scenes matching criteria:\n'
+        for kw in kwargs:
+            if kw == 'intersects':
+                geom = json.dumps(json.loads(kwargs[kw])['geometry'])
+                txt += ('{:>20}: {:<40} ...\n'.format(kw, geom[0:70]))
+            else:
+                txt += ('{:>20}: {:<40}\n'.format(kw, kwargs[kw]))
+        print(txt)
 
-    print('Searching for scenes matching criteria:')
-    for kw in kwargs:
-        print('  %s: %s' % (kw, kwargs[kw]))
-
+    # get scenes from search
     search = Search(**kwargs)
-
-    print('Found %s matching scenes' % search.found())
-
-    # create Scenes collection
     scenes = Scenes(search.scenes())
 
-    if review is not None:
+    if review:
+        if not os.getenv('IMGCAT', None):
+            raise ValueError('Set IMGCAT envvar to terminal image display program to use review feature')
         scenes.review_thumbnails()
 
     # print summary
-    if printsum:
-        scenes.print_summary()
+    if printmd is not None:
+        scenes.print_scenes(printmd)
 
     # print calendar
     if printcal:
@@ -104,13 +43,14 @@ def main(datadir=config.DATADIR, nosubdirs=config.NOSUBDIRS, printsum=False, pri
     # download files given keys
     if download is not None:
         for key in download:
-            scenes.download(key=key, source=source, path=datadir, nosubdirs=nosubdirs)
+            scenes.download(key=key, source=source)
 
     return scenes
 
 
 def cli():
-    args = parse_args(sys.argv[1:])
+    parser = SatUtilsParser(description='sat-search (v%s)' % __version__)
+    args = parser.parse_args(sys.argv[1:])
     scenes = main(**args)
     return len(scenes)
 
--- test/test_main.py
+++ test/test_main.py
@@ -5,36 +5,17 @@ from mock import patch
 import json
 import shutil
 import satsearch.main as main
+import satsearch.config as config
 
 
 testpath = os.path.dirname(__file__)
+config.DATADIR = testpath
 
 
 class Test(unittest.TestCase):
     """ Test main module """
 
     args = '--date 2017-01-01 --satellite_name Landsat-8'.split(' ')
-
-    def test_empty_parse_args(self):
-        """ Parse arguments """
-        args = main.parse_args([])
-        self.assertEqual(len(args), 6)
-        self.assertFalse(args['printsum'])
-        self.assertFalse(args['printcal'])
-
-    def test_parse_args(self):
-        """ Parse arguments """
-        args = main.parse_args(self.args)
-        self.assertEqual(len(args), 8)
-        self.assertEqual(args['date'], '2017-01-01')
-        self.assertEqual(args['satellite_name'], 'Landsat-8')
-
-    def test_parse_args_with_geojson(self):
-        """ Test parsing of arguments with geojson file input """
-        args = ('--intersects %s' % os.path.join(testpath, 'aoi1.geojson')).split(' ')
-        args = main.parse_args(args)
-        aoi = json.loads(args['intersects'])
-        self.assertEqual(aoi['type'], 'Feature')
 
     def test_main(self):
         """ Run main function """
@@ -44,7 +25,7 @@ class Test(unittest.TestCase):
     def test_main_options(self):
         """ Test main program with output options """
         fname = os.path.join(testpath, 'test_main-save.json')
-        scenes = main.main(date='2017-01-01', satellite_name='Landsat-8', save=fname, printcal=True, printsum=True)
+        scenes = main.main(date='2017-01-01', satellite_name='Landsat-8', save=fname, printcal=True, printmd=[])
         self.assertEqual(len(scenes.scenes), 564)
         self.assertTrue(os.path.exists(fname))
         os.remove(fname)
@@ -61,9 +42,7 @@ class Test(unittest.TestCase):
         with open(os.path.join(testpath, 'aoi1.geojson')) as f:
             aoi = json.dumps(json.load(f))
         scenes = main.main(date_from='2017-01-05', date_to='2017-01-21', satellite_name='Landsat-8',
-                           intersects=aoi, download=['thumb', 'MTL'], datadir=testpath)
-
-        self.assertEqual(len(scenes), 2)
+                           intersects=aoi, download=['thumb', 'MTL'])
         for scene in scenes.scenes:
             self.assertTrue(os.path.exists(scene.filenames['thumb']))
             self.assertTrue(os.path.exists(scene.filenames['MTL']))
