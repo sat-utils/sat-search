374a257ab346e4e8845d41c0e455021a8ca9cc50
Matthew Hanson <matt.a.hanson@gmail.com>
2017-10-01 15:04:21 -0400

    cleanup output

--- satsearch/main.py
+++ satsearch/main.py
@@ -1,27 +1,35 @@
 import os
 import sys
 import json
+import logging
 from .version import __version__
 from satsearch import Search, Scenes
 from satsearch.parser import SatUtilsParser
 
 
-def main(review=False, printsearch=False, printmd=None, printcal=False,
-         save=None, download=None, source='aws_s3', **kwargs):
-    """ Main function for performing a search """
-    if printsearch:
-        txt = 'Search for scenes matching criteria:\n'
-        for kw in kwargs:
-            if kw == 'intersects':
-                geom = json.dumps(json.loads(kwargs[kw])['geometry'])
-                txt += ('{:>20}: {:<40} ...\n'.format(kw, geom[0:70]))
-            else:
-                txt += ('{:>20}: {:<40}\n'.format(kw, kwargs[kw]))
-        print(txt)
+logger = logging.getLogger(__name__)
 
-    # get scenes from search
-    search = Search(**kwargs)
-    scenes = Scenes(search.scenes())
+
+def main(review=False, printsearch=False, printmd=None, printcal=False,
+         load=None, save=None, download=None, source='aws_s3', **kwargs):
+    """ Main function for performing a search """
+
+    if load is None:
+        if printsearch:
+            txt = 'Search for scenes matching criteria:\n'
+            for kw in kwargs:
+                if kw == 'intersects':
+                    geom = json.dumps(json.loads(kwargs[kw])['geometry'])
+                    txt += ('{:>20}: {:<40} ...\n'.format(kw, geom[0:70]))
+                else:
+                    txt += ('{:>20}: {:<40}\n'.format(kw, kwargs[kw]))
+            print(txt)
+
+        # get scenes from search
+        search = Search(**kwargs)
+        scenes = Scenes(search.scenes())
+    else:
+        scenes = Scenes.load(load)
 
     if review:
         if not os.getenv('IMGCAT', None):
@@ -40,6 +48,8 @@ def main(review=False, printsearch=False, printmd=None, printcal=False,
     if save is not None:
         scenes.save(filename=save)
 
+    print('%s scenes found' % len(scenes))
+
     # download files given keys
     if download is not None:
         for key in download:
@@ -51,6 +61,10 @@ def main(review=False, printsearch=False, printmd=None, printcal=False,
 def cli():
     parser = SatUtilsParser(description='sat-search (v%s)' % __version__)
     args = parser.parse_args(sys.argv[1:])
+
+    # enable logging
+    logging.basicConfig(stream=sys.stdout, level=args.pop('verbosity') * 10)
+
     scenes = main(**args)
     return len(scenes)
 
